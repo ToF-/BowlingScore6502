301a35f8427b210e55a8d910d56cc4d35359000d
ToF- <cthibaut@octo.com>
2015-02-02 23:58:34 +0100

    calling from a basic program

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 bowling:	bowling.asm
-	dasm bowling.asm -obowling.prg -lbowling.txt -v3
+	dasm bowling.asm -obowling.prg -sbowling.sym -v3 -T0
--- bowling.asm
+++ bowling.asm
@@ -1,19 +1,46 @@
     processor 6502
+    org $0801
+    .byte $0c, $08, $0a, $00, $9e, $20
+    .byte $34, $30, $39, $36, $00, $00
+    .byte $00
     org $1000
 
+    jmp tests
 add_roll
     lda bonus
-    and #$03
+    and #$03   
     tax 
 loop_score
     jsr add_score
     dex
-    bne loop_score
-    lda frame
-    cmp #$10
-    beq end_score
+    bne loop_score 
+    lda frame   
+    cmp #$10    
+    beq add_done
     jsr add_score
-end_score
+add_done
+    clc
+    lda track
+    adc roll
+    cmp #21     ;  11 + 10 = 10 roll on a new frame = strike
+    bne not_strike
+    lda #3
+    sta type
+    rts
+not_strike
+    cmp #$0A    ; tr + ro = 10 it's a spare
+    bne not_spare
+    lda #2
+    sta type
+    rts
+not_spare
+    bcc not_second
+    lda #1
+    sta type
+    rts
+not_second
+    lda #0
+    sta type
     rts
 add_score
     clc
@@ -35,10 +62,50 @@ start
     lda #$0B
     sta track
     rts
+tests
+    lda #$0B
+    sta track
+    lda #$0A
+    sta roll
+    jsr add_roll
+    lda type
+    cmp #$03
+    bne fail
+    jsr succeed
+    
+    lda #$04
+    sta track
+    lda #$06
+    sta roll
+    jsr add_roll
+    lda type
+    cmp #$02
+    bne fail
+    jsr succeed
+    
+    lda #$0B
+    sta track
+    lda #$04
+    sta roll
+    jsr add_roll
+    lda type
+    cmp #$00
+    bne fail 
+    jsr succeed
+    rts
+succeed
+    lda #5
+    sta $d020
+    rts
+fail
+    lda #2
+    sta $d020
+    rts
 
 roll    .byte 0
 score   .word 0
 bonus   .byte 0
 frame   .byte 0
 track   .byte 0
+type    .byte 0
      
