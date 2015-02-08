0d07c0c6bf8424013b63164971862c5839f6dc1f
ToF- <cthibaut@octo.com>
2015-02-08 21:13:12 +0100

    clear screen

---
+++ code/main.asm
@@ -0,0 +1,14 @@
+; main.asm
+
+chrout = $ffd2
+border_col = $d020
+bckgnd_col = $d021
+
+            lda #0          ; black
+            sta border_col
+            sta bckgnd_col
+            lda #147        ; clear screen
+            jsr chrout
+            lda #30         ; set cursor color to green
+            jsr chrout
+            rts
